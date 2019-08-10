680b879a85f6b818a77fec20915b7f69e1555891
estefa942 <estefany.muriel@udea.edu.co>
2019-08-10 10:21:12 -0500

    heroku

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname+'/dist/ng-blog'));
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
