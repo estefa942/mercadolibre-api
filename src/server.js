514dc26ffd9a04a76e2d31ca7ba2c872feea21b8
estefa942 <estefany.muriel@udea.edu.co>
2019-08-10 10:45:07 -0500

    ahora si herokuu

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/ng-blog'));
+app.use(express.static(__dirname+'/dist/mercadolibre'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/mecadolibre/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
